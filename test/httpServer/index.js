61b3ce8293155cc6aa559293ad7603f99af1e339
Conrado Quilles Gomes <conradoqg@gmail.com>
2018-02-18 17:46:33 -0300

    Improve tests adding exception and user verification

--- test/httpServer/index.js
+++ test/httpServer/index.js
@@ -16,13 +16,25 @@ describe('HTTPServer', async () => {
 
     before(async () => {
         const dockerService = new Docker();
-        const dbService = new DB();
-        const authService = new Auth(dbService);
-        const server = new HTTPServer(dockerService, authService, dbService);
+        context.dbService = new DB();
+        const authService = new Auth(context.dbService);
+        const server = new HTTPServer(dockerService, authService);
         context.server1 = server;
-        context.userUser = {
+        context.userInvalid = {
             email: chance.email({ domain: 'totvs.com.br' }),
-            password: chance.string()
+            password: chance.string({ length: 5 })
+        };
+        context.userUser1 = {
+            email: chance.email({ domain: 'totvs.com.br' }),
+            password: chance.string({ length: 6 })
+        };
+        context.userUser2 = {
+            email: chance.email({ domain: 'totvs.com.br' }),
+            password: chance.string({ length: 6 })
+        };
+        context.userAdmin1 = {
+            email: 'conrado.gomes@totvs.com.br',
+            password: '123456'
         };
         return await server.listen('localhost', '80');
     });
@@ -47,34 +59,71 @@ describe('HTTPServer', async () => {
             .expect(401);
     });
 
-    step('create a user', async () => {
+    step('create user 1', async () => {
         return await supertest(context.server1.app)
             .post('/users')
-            .send(context.userUser)
+            .send(context.userUser1)
             .expect(200);
     });
 
-    step('get token', async () => {
+    step('create user 2', async () => {
+        return await supertest(context.server1.app)
+            .post('/users')
+            .send(context.userUser2)
+            .expect(200);
+    });
+
+    step('create user admin 1', async () => {
+        return await supertest(context.server1.app)
+            .post('/users')
+            .send(context.userAdmin1)
+            .expect(200);
+    });
+
+    step('create invalid user', async () => {
+        return await supertest(context.server1.app)
+            .post('/users')
+            .send(context.userInvalid)
+            .expect(400);
+    });
+
+    step('get token for user 1', async () => {
         const tokenResponse = await supertest(context.server1.app)
             .post('/token')
-            .send(context.userUser)
+            .send(context.userUser1)
             .expect(200);
-        context.tokenUser = tokenResponse.body.token;
+        context.tokenUserUser1 = tokenResponse.body.token;
     });
 
-    step('get stacks of an user', async () => {
+    step('get token for user 2', async () => {
+        const tokenResponse = await supertest(context.server1.app)
+            .post('/token')
+            .send(context.userUser2)
+            .expect(200);
+        context.tokenUserUser2 = tokenResponse.body.token;
+    });
+
+    step('get token for user admin 1', async () => {
+        const tokenResponse = await supertest(context.server1.app)
+            .post('/token')
+            .send(context.userAdmin1)
+            .expect(200);
+        context.tokenUserAdmin1 = tokenResponse.body.token;
+    });
+
+    step('get stacks of user 1', async () => {
         const stacksResult = await supertest(context.server1.app)
             .get('/stacks')
-            .set({ Authorization: context.tokenUser })
+            .set({ Authorization: context.tokenUserUser1 })
             .expect(200);
         stacksResult.body.should.be.an('array').that.is.empty;
     });
 
-    step('create a stack for an user', async (done) => {
+    step('create stack 1 for user 1', async (done) => {
         try {
             const stackCreationResult = await supertest(context.server1.app)
                 .post('/stacks')
-                .set({ Authorization: context.tokenUser })
+                .set({ Authorization: context.tokenUserUser1 })
                 .send({ composeTemplateName: 'docker-stack-sample1' })
                 .expect(200);
             stackCreationResult.should.not.be.empty;
@@ -86,25 +135,84 @@ describe('HTTPServer', async () => {
         } catch (ex) {
             done(ex);
         }
-    }).timeout(15000);
+    }).timeout(999999);
 
-    step('get the created stack of an user', async () => {
+    step('create stack 2 for user 1', async (done) => {
+        try {
+            const stackCreationResult = await supertest(context.server1.app)
+                .post('/stacks')
+                .set({ Authorization: context.tokenUserUser1 })
+                .send({ composeTemplateName: 'docker-stack-sample1' })
+                .expect(200);
+            stackCreationResult.should.not.be.empty;
+            stackCreationResult.body.should.not.be.empty;
+            stackCreationResult.body.should.have.property('stackName');
+            context.stack2 = stackCreationResult.body.stackName;
+            await timeout(5000);
+            done();
+        } catch (ex) {
+            done(ex);
+        }
+    }).timeout(999999);
+
+    step('create stack 3 for user 1 (should not create)', async () => {
+        await supertest(context.server1.app)
+            .post('/stacks')
+            .set({ Authorization: context.tokenUserUser1 })
+            .send({ composeTemplateName: 'docker-stack-sample1' })
+            .expect(400);
+    }).timeout(999999);
+
+    step('get the created stack 1 of user 1', async () => {
         const stackResult = await supertest(context.server1.app)
             .get(`/stacks/${context.stack1}`)
-            .set({ Authorization: context.tokenUser })
+            .set({ Authorization: context.tokenUserUser1 })
             .expect(200);
         stackResult.body.should.not.be.empty;
     });
 
-    step('delete the created stack of an user', async () => {
+    step('get the created stack 1 of user 1 from user 2 (should not get)', async () => {
+        await supertest(context.server1.app)
+            .get(`/stacks/${context.stack1}`)
+            .set({ Authorization: context.tokenUserUser2 })
+            .expect(404);
+    });
+
+    step('get the created stacks from admin', async () => {
         const stackResult = await supertest(context.server1.app)
-            .delete(`/stacks/${context.stack1}`)
-            .set({ Authorization: context.tokenUser })
+            .get('/stacks/')
+            .set({ Authorization: context.tokenUserAdmin1 })
             .expect(200);
         stackResult.body.should.not.be.empty;
+        stackResult.body.should.be.an('array').that.is.not.empty;
+    });
+
+    step('delete the created stack 1 of user 1', async () => {
+        const stackResult = await supertest(context.server1.app)
+            .delete(`/stacks/${context.stack1}`)
+            .set({ Authorization: context.tokenUserUser1 })
+            .expect(200);
+        stackResult.body.should.not.be.empty;
+    });
+
+    step('delete the created stack 2 of user 1', async () => {
+        const stackResult = await supertest(context.server1.app)
+            .delete(`/stacks/${context.stack2}`)
+            .set({ Authorization: context.tokenUserUser1 })
+            .expect(200);
+        stackResult.body.should.not.be.empty;
+    });
+
+    step('try to create a invalid stack for user 1', async () => {
+        await supertest(context.server1.app)
+            .post('/stacks')
+            .set({ Authorization: context.tokenUserUser1 })
+            .send({ composeTemplateName: 'this-does-not-exist' })
+            .expect(400);
     });
 
     after(async () => {
+        await context.dbService.destroy();
         return await context.server1.stop();
     });
 });
